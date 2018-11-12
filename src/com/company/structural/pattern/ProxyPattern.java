2d592dea58b9fe7b72430b180ce2140947409cad
C先生 <1439265824@qq.com>
2018-11-12 18:17:01 +0800

    新增代理模式的例子 代理模式分为三个部分： Subject:抽象主题角色 Proxy:代理主题角色 RealSubject:真实主题角

--- src/com/company/structural/pattern/ProxyPattern.java
+++ src/com/company/structural/pattern/ProxyPattern.java
@@ -3,4 +3,48 @@ package com.company.structural.pattern;
 
 /*代理模式*/
 public class ProxyPattern {
+
+    /** 代理模式包含如下角色：
+     * Subject:抽象主题角色
+     * Proxy:代理主题角色
+     * RealSubject:真实主题角色
+     **/
+
+    /*抽象对象角色*/
+    public abstract class AbstractObject{
+        //操作
+        public abstract void operation();
+    }
+
+    /*目标对象角色*/
+    public class RealObject extends AbstractObject{
+        @Override
+        public void operation() {
+            //操作代码
+            System.out.println("执行一些操作！");
+        }
+    }
+
+    /*代理对象角色*/
+    public class ProxyObject extends AbstractObject{
+
+        RealObject realObject = new RealObject();
+
+        @Override
+        public void operation() {
+            //调用目标对象之前可以做相关操作
+            System.out.println("before");
+            realObject.operation();
+            //调用目标对象之后可以做相关操作
+            System.out.println("after");
+        }
+    }
+
+    /*客户端实现*/
+    public class Client{
+        public Client(){
+            AbstractObject obj = new ProxyObject();
+            obj.operation();
+        }
+    }
 }
